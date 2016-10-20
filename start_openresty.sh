8c0c139c0688adab6569d5b4fdb93036b079d734
David Pollak <feeder.of.the.bears@gmail.com>
2016-10-20 15:35:29 -0700

    Service discovery of rabbitmq on Mesos cluster

--- start_openresty.sh
+++ start_openresty.sh
@@ -9,34 +9,36 @@ fi
 
 
 if [ ! -z "$MESOS_TASK_ID" ]; then
-    echo "Hostname " $(hostname) >&2
-
     HOST=""
     PORT=""
 
     host () {
-        HOST=$(dig _woof-woofer1-dogs._tcp.marathon.mesos SRV | \
+        HOST=$(dig _rabbit-funcatron._tcp.marathon.mesos SRV | \
                       grep "IN A" | grep -v "^\;" | \
                       grep -o "[0-9]*\.[0-9]*\.[[0-9]*\.[0-9]*$" | \
                       sed -n 1p)
     }
 
     port () {
-        PORT=$(dig _woof-woofer1-dogs._tcp.marathon.mesos SRV | \
-                      grep "IN SRV" | grep -v "^\;" | \
-                      grep -o "[0-9]* *[^ ]*$" | grep -o "^[0-9]*" | \
-                      sed -n 1p)
+        PORT=$(dig _stomp._rabbit-funcatron._tcp.marathon.mesos SRV | \
+                      grep -v "^\;" | grep -e "IN\s\+SRV" | \
+                      grep -o "[0-9]* *[^ ]*$" | \
+                      grep -o "^[0-9]*" | sed -n 1p)
     }
 
-
-
-    while [ true ] ; do
+    while [[ -z "$PORT" || -z "$HOST" ]] ; do
+        sleep 1
         host
         port
-        echo "Hello Marathon $HOST $PORT" >&2
-        sleep 5
     done
-    # FIXME if in mesos and RABBIT_HOST/RABBIT_PORT not defined, use dig to find/define
+
+    export RABBIT_HOST=$HOST
+    export RABBIT_PORT=$PORT
+
+    #while [ true ] ; do
+    #    echo "Hello Marathon $HOST $PORT" >&2
+    #    sleep 5
+    #done
 fi
 
 
