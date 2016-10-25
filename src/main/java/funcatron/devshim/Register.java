493ca95539ac92cb48ffadf5de695af560a2651a
David Pollak <feeder.of.the.bears@gmail.com>
2016-10-25 16:50:47 -0700

    Got devshim mostly working

--- src/main/java/funcatron/devshim/Register.java
+++ src/main/java/funcatron/devshim/Register.java
@@ -27,11 +27,62 @@ import java.util.logging.Logger;
  */
 public class Register {
 
-    static class Mooser implements Func<Map, Map> {
+
+    public static class User implements java.io.Serializable {
+
+        private String name;
+        private Integer age;
+
+        public String getName(){
+            return this.name;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public Integer getAge(){
+            return this.age;
+        }
+
+        public void setAge(Integer age){
+            this.age = age;
+        }
+    }
+
+    public static class PetOwner implements java.io.Serializable {
+        private User pet;
+
+        public PetOwner(User pet) {
+            this.pet = pet;
+        }
+
+        public PetOwner() {
+
+        }
+
+        public User getPet() {return pet;}
+        public void setPet(User u) {pet = u;}
+    }
+
+    static class Mooser implements Func<PetOwner, User> {
 
         @Override
-        public Map apply(Map map, Context context) {
-            return new HashMap();
+        public User apply(PetOwner po, Context context) {
+
+            User ret = null;
+
+            if (null != po) ret = po.getPet();
+
+            if (ret == null) {
+                ret = new User();
+                ret.setAge(52);
+                ret.setName("David");
+            }
+            ret.setName("Hello: "+ret.getName());
+            ret.setAge(1 + ret.getAge());
+
+            return ret;
         }
     }
 
@@ -130,7 +181,6 @@ public class Register {
      * @return
      */
     private static Void invoker(Map<String, Object> info) {
-        logger.log(Level.SEVERE, "Invoker");
         String classname = (String) info.get("class");
         Map<String, Object> headers = (Map<String, Object>) info.get("headers");
 
@@ -141,24 +191,43 @@ public class Register {
                     Arrays.stream(c.getMethods()).filter(m -> m.getName().equals("apply") &&
                             m.getParameterCount() == 2).findFirst().get();
             Class paramType = meth.getParameterTypes()[0];
+            Object theParam = null;
+
+            if (null != info.get("body")) {
+                theParam = (new ObjectMapper()).
+                        readValue((String) info.get("body"), paramType);
+            }
+
             Object ret = f.apply(
-                    (new ObjectMapper()).reader().forType(paramType).readValue(Base64.getDecoder().decode((String) info.get("body"))),
+                    theParam,
                     new ContextImpl(headers, LoggerFactory.getLogger(classname)));
 
             HashMap<String, Object> answer = new HashMap<>();
+            HashMap<String, Object> response = new HashMap<>();
+            HashMap<String, Object> repHeaders = new HashMap<>();
+
             answer.put("cmd", "reply");
             answer.put("replyTo", info.get("replyTo"));
-            answer.put("status", 200);
+            answer.put("response", response);
+
+            response.put("status", 200);
+            response.put("headers", repHeaders);
+
 
             if (null == ret) {
 
+                repHeaders.put("Content-Type", "text/plain");
+                response.put("body", "");
+
             } else if (ret instanceof MetaResponse) {
                 MetaResponse mr = (MetaResponse) ret;
-                answer.put("headers",mr.getHeaders());
-                answer.put("status", mr.getResponseCode());
-                answer.put("body", Base64.getEncoder().encode(mr.getBody()));
+                response.put("headers",mr.getHeaders());
+                response.put("status", mr.getResponseCode());
+                response.put("body", Base64.getEncoder().encode(mr.getBody()));
+                answer.put("decodeBody", true);
             } else {
-                answer.put("body", Base64.getEncoder().encode((new ObjectMapper()).writer().writeValueAsBytes(ret)));
+                response.put("body", ret);
+                repHeaders.put("Content-Type", "application/json");
             }
 
             sendMessage(answer);
@@ -166,12 +235,19 @@ public class Register {
         } catch (Exception e) {
             logger.log(Level.WARNING, e, () -> "Unabled to invoke " + classname);
             HashMap<String, Object> answer = new HashMap<>();
+            HashMap<String, Object> response = new HashMap<>();
+            HashMap<String, Object> repHeaders = new HashMap<>();
+
+            answer.put("response", response);
+            response.put("headers", repHeaders);
+
             answer.put("cmd", "reply");
             answer.put("replyTo", info.get("replyTo"));
-            answer.put("status", 500);
 
+            response.put("status", 500);
+            repHeaders.put("Content-Type", "text/plain");
             try {
-                answer.put("body", Base64.getEncoder().encode(e.getMessage().getBytes("UTF-8")));
+                response.put("body", e.getMessage());
                 sendMessage(answer);
             } catch (IOException ioe) {
                 logger.log(Level.WARNING, ioe, () -> "Failed to send response");
@@ -224,21 +300,13 @@ public class Register {
     }
 
     private static void processMessages(long version) {
-        logger.log(Level.SEVERE, "in Got a line");
-        System.out.println("in Got line ");
-        System.err.println("in err Got line ");
         try {
             InputStreamReader ios = new InputStreamReader(tronInput, "UTF-8");
             BufferedReader br = new BufferedReader(ios);
             while (version == runCount.longValue()) {
                 String line = br.readLine();
-                logger.log(Level.SEVERE, "Got a line");
-                System.out.println("Got line "+line);
-                System.err.println("err Got line "+line);
                 byte[] bytes = Base64.getDecoder().decode(line);
                 final Map message = (new ObjectMapper()).reader().forType(Map.class).readValue(bytes);
-
-                logger.log(Level.SEVERE, "Hey");
 
                 String cmd = (String) message.get("cmd");
                 if (null != cmd) {
