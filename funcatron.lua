3f0ca9ccc60b244f065a16e638f58a4b204060f1
David Pollak <feeder.of.the.bears@gmail.com>
2016-11-30 13:58:23 -0800

    Change the tron queue to for_tron

--- funcatron.lua
+++ funcatron.lua
@@ -31,7 +31,7 @@ local bunny_user = os.getenv("RABBIT_USER") or "guest"
 
 local bunny_pwd = os.getenv("RABBIT_PWD") or "guest"
 
-local tron_queue = os.getenv("TRON_QUEUE") or "tron"
+local tron_queue = os.getenv("TRON_QUEUE") or "for_tron"
 
 local dev_mode = os.getenv("DEV_MODE") or true
 
