460c87a28806e4d62b65df8b6af18f02361c43a2
David Pollak <feeder.of.the.bears@gmail.com>
2016-12-21 11:11:50 -0800

    Added outlines of other language projects

---
+++ clojure_sample/src/clojure_sample/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-sample.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ clojure_sample/test/clojure_sample/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-sample.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-sample.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
