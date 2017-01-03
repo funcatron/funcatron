db96b9bd6540bf6d9180ecc68be4cbc0a6b0aa4f
David Pollak <feeder.of.the.bears@gmail.com>
2017-01-02 17:24:02 -0800

    Updated to the 0.2.1 API

--- src/main/resources/archetype-resources/src/main/java/MyFunction.java
+++ src/main/resources/archetype-resources/src/main/java/MyFunction.java
@@ -3,32 +3,87 @@ package $package;
 import funcatron.intf.Func;
 import funcatron.intf.Context;
 import funcatron.devshim.Register;
+
 import java.io.File;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.Date;
+import redis.clients.jedis.Jedis;
 
 /**
  * Hello world!
- *
  */
-public class MyFunction implements Func<MyPojo, MyPojo>
-{
+public class MyFunction implements Func<MyPojo> {
     /**
      * Implement your business logic here
-     * @param pojo the incoming request parameters, converted by Jackson to your class
+     *
+     * @param pojo    the incoming request parameters, converted by Jackson to your class
      * @param context the context of the request
      * @return the result
      */
     @Override
-    public MyPojo apply(MyPojo pojo, Context context) {
+    public Object apply(MyPojo pojo, final Context context) throws Exception {
         if (null == pojo) {
             pojo = new MyPojo();
             pojo.setName("Example");
             pojo.setAge(42);
         }
 
-        pojo.setName("Hello: "+pojo.getName());
+        // if we have a Redis driver, let the world know
+        context.vendForName("cache", Jedis.class).toStream().forEach(a ->
+        {
+            context.getLogger().log(Level.INFO, "Yay!. Got Redis Driver");
+            return null;
+        });
+
+        pojo.setName("Hello: " + pojo.getName() + " at " + (new Date()));
         pojo.setAge(pojo.getAge() + 1);
+        // put the pojo in the DB
+        addToDatabase(pojo, context);
         return pojo;
     }
+
+    /**
+     * Add the pojo to the database
+     *
+     * @param pojo the Pojo to add
+     * @param c    the context
+     */
+    private void addToDatabase(MyPojo pojo, Context c) {
+        try {
+            // get the DB connection
+            c.vendForName("db", Connection.class).
+                    map((Connection db) -> {
+                        try {
+                            // create the table if it doesn't exist
+                            Statement ct = db.createStatement();
+                            ct.execute("CREATE TABLE IF NOT EXISTS pojos(NAME VARCHAR(8192), AGE BIGINT);");
+
+                            // insert the pojo
+                            PreparedStatement st = db.prepareStatement("INSERT INTO pojos (name, age) VALUES (?, ?)");
+                            st.setString(1, pojo.getName());
+                            st.setInt(2, pojo.getAge());
+                            st.executeUpdate();
+                            st.close();
+
+                            // get the pojo count and display it
+                            ResultSet rs = ct.executeQuery("SELECT COUNT(*) FROM pojos");
+                            if (rs.next()) {
+                                Object cnt = rs.getObject(1);
+                                c.getLogger().log(Level.INFO, "There are " + cnt + " rows in the DB");
+                            }
+                            rs.close();
+                            ct.close();
+                        } catch (SQLException se) {
+                            c.getLogger().log(Level.WARNING, "Failed to insert pojo", se);
+                        }
+                        return null;
+                    });
+        } catch (Exception e) {
+            c.getLogger().log(Level.WARNING, "Failed to add pojo to db", e);
+        }
+    }
+
 
     private static String funcatronDevHost() {
         return "localhost";
@@ -41,15 +96,16 @@ public class MyFunction implements Func<MyPojo, MyPojo>
     /**
      * Call this method during development, but not production, to
      * hook up to the Funcatron
+     *
      * @param args
      * @throws Exception
      */
-    public static void main( String[] args ) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         System.out.println("Starting connection to Funcatron dev server");
         System.out.println("run the Funcatron dev server with: docker run -ti --rm  -p 3000:3000 -p 54657:54657 funcatron/dev-docker:latest");
 
         Register.register(funcatronDevHost(), funcatronDevPort(),
-                new File("src/main/resources/funcatron.yaml"));
+                new File("src/main/resources/funcatron.yaml"),
+                new File("src/main/resources/exec_props.json"));
     }
 }
