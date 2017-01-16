fbf879d49a5b6b58e57a6380358b128894e03be5
David Pollak <feeder.of.the.bears@gmail.com>
2017-01-16 11:24:26 -0800

    Added a script to pull all the repos

---
+++ doc_o_matic/pull_all.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ROOT="${DIR}/../.."
+
+for x in "funcatron" "intf" "starter" "devshim" "tron" "samples" "jvm_services"; do
+    echo "Doing ${x}"
+    cd "${ROOT}/${x}" || exit 1
+    git pull || exit 1
+done
+
+echo "Yay! Done pulling all the repos"
