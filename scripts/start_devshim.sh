76121b705221ac6d9962b5e6491f3563a97f2303
David Pollak <feeder.of.the.bears@gmail.com>
2017-02-14 13:21:44 -0800

    Added devshim starter script

---
+++ scripts/start_devshim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker pull funcatron/tron:v0.2.6
+
+docker inspect func-devshim > /dev/null 2> /dev/null || \
+    docker run -ti --rm -p 3000:3000  \
+           -p 54657:54657 \
+           -e TRON_1=--devmode \
+           --name func-devshim funcatron/tron:v0.2.6
