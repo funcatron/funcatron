7d5bc589cc3372ffbbe2751b658eb2732e069f7b
David Pollak <feeder.of.the.bears@gmail.com>
2017-05-14 11:49:18 -0700

    Okay... we should autogenerate and autodeploy docs on each build

--- doc_o_matic/build-docs.sh
+++ doc_o_matic/build-docs.sh
@@ -1,11 +1,31 @@
 #!/usr/bin/env bash
 
+
 echo "Building Doc-o-Matic container"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -t funcatron/doc-o-matic:latest . || exit 1
 
-docker run -ti --rm --net=host -v $(cd ${DIR}/../.. && pwd):/data funcatron/doc-o-matic:latest /usr/bin/doc_it.py
+docker run -ti --rm --net=host -v $(cd ${DIR}/../.. && pwd):/data funcatron/doc-o-matic:latest /usr/bin/doc_it.py || exit 1
+
+if [[ $PUSH_DOCS || $TRAVIS_SECURE_ENV_VARS ]]; then
+    if [[ $TRAVIS_SECURE_ENV_VARS ]]; then
+        echo "Copying ssh deploy key"
+        mkdir ~/.ssh
+        cp $DIR/../id_ed25519 ~/.ssh
+        chmod -R og-rwx ~/.ssh
+    fi
+
+    THE_DATE=$(date +"%Y%m%d%H%M%S")
+    NEW_PLACE="funcatron_${THE_DATE}"
+    cd $DIR
+    echo "Copying docs"
+    cat ../../doc.tgz | \
+        ssh -oStrictHostKeyChecking=no ubuntu@funcatron.org \
+            "tar -xzf - ;  mkdir ${NEW_PLACE} ; mv docout ${NEW_PLACE}/ ; rm funcatron ; ln -s ${NEW_PLACE} funcatron" || exit 1
+    echo "Deposited docs"
+fi
+
 
 echo $?
