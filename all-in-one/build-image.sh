6c1cc54aac897881f949dd0a6f9daf6d61a0efd1
David Pollak <feeder.of.the.bears@gmail.com>
2017-06-01 23:43:47 +0200

    Fixed a Docker build issue for all-in-one and all-in-one builds the local repo's Tron

--- all-in-one/build-image.sh
+++ all-in-one/build-image.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build -t funcatron/allinone:latest .
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${DIR}/.."
+
+docker build -f all-in-one/Dockerfile -t funcatron/allinone:latest .
